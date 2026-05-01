dfb751d563a3455be2657c7d0626fbe79a9fe2cc
KokoGrth <kokoanugrahpratama@gmail.com>
2026-05-01 20:20:49 +0800

    Belajar Html & CSS

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,3 +1,4 @@
+/*
 export default function learnpage() {
     return (
     <div>
@@ -12,5 +13,41 @@ export default function learnpage() {
     </div>
 );
 }
+*/
 
-
+export default function learnpage() {
+    return (
+    <section className="h-screen bg-gray-200 flex gap-4 p-8"> 
+    <div className = "bg-purple-300 p-8 h-72 w-96 rounded-4xl flex flex-col justify-between text-blue-950">
+        <div className = "flex gap-4" >
+            <img src="/paint.svg" alt="design" />
+            <p className="font-bold">Design</p>
+        </div>
+        <div>
+            <p className="text-4xl font-bold">Figma</p>
+            <p>8 Hours</p>
+        </div>
+    </div>
+    <div className = "bg-orange-300 p-8 h-72 w-96 rounded-4xl flex flex-col justify-between text-orange-800">
+        <div className = "flex gap-4" >
+            <img src="/paint.svg" alt="design" />
+            <p className="font-bold">Codding</p>
+        </div>
+        <div>
+            <p className="text-4xl font-bold">Python</p>
+            <p>2 Hours</p>
+        </div>
+    </div>
+    <div className = "bg-pink-300 p-8 h-72 w-96 rounded-4xl flex flex-col justify-between text-pink-800">
+        <div className = "flex gap-4" >
+            <img src="/paint.svg" alt="design" />
+            <p className="font-bold">Design</p>
+        </div>
+        <div>
+            <p className="text-4xl font-bold">Sketch</p>
+            <p>4 Days</p>
+        </div>
+    </div>
+    </section>
+);
+}
